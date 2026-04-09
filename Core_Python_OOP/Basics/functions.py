2feb008b4db5a71c0cdf33890ff8c9c273661e87
Bharath chikkala <bharatchikkala@gmail.com>
2026-04-10 00:58:12 +0530

    Implement name formatting and greeting functions
    Added functions to format names and print greetings.

--- Core_Python_OOP/Basics/functions.py
+++ Core_Python_OOP/Basics/functions.py
@@ -14,3 +14,17 @@ greet("sravani")
 def function_name(name, location):
     print(f"My name is {name} and Iam from {location}")
 function_name(name="ganga", location= "Amp")
+
+#function with outputs
+
+def format_name(f_name, l_name):
+    print(f_name.title())
+    print(l_name.title())
+
+format_name("BhARATh","ganGA")
+
+def ganga(f_name, l_name):
+    formatted_f = f_name.title()
+    formatted_l = l_name.title()
+    return f"{formatted_f} {formatted_l}"
+print(ganga("BhARATh","ganGA"))
