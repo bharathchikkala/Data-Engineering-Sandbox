d4fcc06415b90d8b62aeab81b562a41b7ae3476b
Bharath chikkala <bharatchikkala@gmail.com>
2026-04-10 00:04:27 +0530

    Update functions.py

--- Core_Python_OOP/Basics/functions.py
+++ Core_Python_OOP/Basics/functions.py
@@ -1,1 +1,15 @@
+#Function with inputs
 
+def greet(name):
+    print(f"Hey! {name}")
+    print(f"How r u {name}")
+    print(f"where r u {name}")
+greet("ganga")
+greet("bharath")
+greet("sravani")
+
+#Positional vs Keyword arguments
+
+def function_name(name, location):
+    print(f"My name is {name} and Iam from {location}")
+function_name(name="ganga", location= "Amp")
