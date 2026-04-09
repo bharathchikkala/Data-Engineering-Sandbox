feea4de607dd6c5a8c3aa19b617e9e53557c2c46
Bharath chikkala <bharatchikkala@gmail.com>
2026-04-10 01:10:17 +0530

    Implement Higher or Lower game logic

--- Core_Python_OOP/Mini Projects/Higher or Lower Project/main.py
+++ Core_Python_OOP/Mini Projects/Higher or Lower Project/main.py
@@ -1,1 +1,52 @@
+import random
+from game_data import data
+a = random.choice(data)
+current_score = 0
+game_starts = True
+while game_starts:
+    logo = r"""
+        __  ___       __             
+       / / / (_)___ _/ /_  ___  _____
+      / /_/ / / __ `/ __ \/ _ \/ ___/
+     / __  / / /_/ / / / /  __/ /    
+    /_/ ///_/\__, /_/ /_/\___/_/     
+       / /  /____/_      _____  _____
+      / /   / __ \ | /| / / _ \/ ___/
+     / /___/ /_/ / |/ |/ /  __/ /    
+    /_____/\____/|__/|__/\___/_/     
+    """
+    print(logo)
+    b = random.choice(data)
+    print(f"Compare A: {a['name']}, {a['description']}, from {a['country']}")
+    vs = r"""
+     _    __    
+    | |  / /____
+    | | / / ___/
+    | |/ (__  ) 
+    |___/____(_)
+    """
+
+    print(vs)
+    print(f"Against B: {b['name']}, {b['description']}, from {b['country']}")
+    print(f"Your current score is:{current_score}")
+    Answer = input("Who has more followers? type 'A' or 'B': ").upper()
+    A = a['follower_count']
+    B = b['follower_count']
+    if Answer == 'A' and A > B:
+        current_score += 1
+        print(f"U got it, Current score is:{current_score}")
+        a = b
+    elif Answer == 'A' and A < B:
+        print("It was not! TRY AGAIN")
+        game_starts = False
+    elif Answer == 'B' and A < B:
+        current_score += 1
+        print(f"U got it, Current score is:{current_score}")
+        a = b
+    elif Answer == 'B' and A > B:
+        print("It was not! TRY AGAIN")
+        game_starts = False
+
+print(f"Your FINAL SCORE is:{current_score}")
+
 
