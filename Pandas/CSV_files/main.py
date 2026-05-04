b0d5f5a241f8b5f901c3c6cfb4c2c85c7d4adeed
Bharath chikkala <bharatchikkala@gmail.com>
2026-05-04 13:48:55 +0530

    Read temperature data from CSV file
    Added functionality to read temperature data from weather_data.csv and store it in a list.

--- Pandas/CSV_files/main.py
+++ Pandas/CSV_files/main.py
@@ -2,3 +2,14 @@
 with open("weather_data.csv") as csvfile:
     file = csvfile.readlines()
     print(file)
+
+#byusing csv inbuilt library getting temp data into list
+import csv
+
+with open("weather_data.csv") as csvfile:
+    data = csv.reader(csvfile)
+    temperatures = []
+    for row_temp in data:
+        if row_temp[1] != "temp":
+            temperatures.append(int(row_temp[1]))
+    print(temperatures)
