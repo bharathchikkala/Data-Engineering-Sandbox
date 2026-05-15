1251de08080f482ffdf56a49128b3307b15b01a5
Bharath chikkala <bharatchikkala@gmail.com>
2026-05-15 16:07:10 +0530

    Clarify 'cnt' parameter in weather API request
    Added a comment to clarify the 'cnt' parameter in the weather API request.

--- API/Weather_API.py
+++ API/Weather_API.py
@@ -2,6 +2,7 @@
 For weather documention go through this link: https://openweathermap.org/api/current?collection=current_forecast
 For API_KEY login to above link and get your unique key
 To find your latitud and longitude of your place: https://www.latlong.net/
+To view your total data in json format: https://jsonviewer.stack.hu/
 '''
 
 import requests
@@ -9,7 +10,7 @@ weather_parameters = {
     "lat" : 16.572090,
     "lon" : 82.000854,
     "appid" : "985dbb049de5ec1aa9b90edfb1cf7257",
-    "cnt" : 10
+    "cnt" : 10  #1 cnt means 3hr of weather data in a day 
 }
 response = requests.get(url="http://api.openweathermap.org/data/2.5/forecast",params=weather_parameters)
 response.raise_for_status()
