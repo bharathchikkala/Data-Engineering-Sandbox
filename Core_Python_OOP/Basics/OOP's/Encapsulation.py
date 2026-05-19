17839bd27ee69f8c480476e6282428027a94654b
Bharath chikkala <bharatchikkala@gmail.com>
2026-05-19 23:57:30 +0530

    Implement Bank class with encapsulation example
    Added a Bank class to demonstrate encapsulation with balance management methods.

--- Core_Python_OOP/Basics/OOP's/Encapsulation.py
+++ Core_Python_OOP/Basics/OOP's/Encapsulation.py
@@ -1,1 +1,25 @@
+#Understanding Encapsulation, like it mainly for to protect data which is inside the class
 
+class Bank:
+
+    def __init__(self):
+        self.__balance = 2000
+
+    def show_balance(self):
+        print(self.__balance)
+
+    def deposit(self,amount):
+        self.__balance += amount
+
+    def withdraw(self,withdrawn_amount):
+        self.__balance -= withdrawn_amount
+
+
+bank_obj = Bank()
+
+bank_obj.show_balance()
+bank_obj.deposit(amount=1500)
+bank_obj.show_balance()
+bank_obj.withdraw(withdrawn_amount=1000)
+bank_obj.balance = 0
+bank_obj.show_balance()
