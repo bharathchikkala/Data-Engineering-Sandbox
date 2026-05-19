9c793843102b7b33f00459ae6ffe4d9b94f3e545
Bharath chikkala <bharatchikkala@gmail.com>
2026-05-20 00:25:59 +0530

    Implement polymorphism with Employee subclasses
    Added examples of polymorphism with Employee, Developer, and Tester classes demonstrating method overriding.

--- Core_Python_OOP/Basics/OOP's/Polymorphism.py
+++ Core_Python_OOP/Basics/OOP's/Polymorphism.py
@@ -1,1 +1,32 @@
+#Polymorphism, mainly for like it allows 2 classes use the same name methods but perform different actios
 
+class Employee():
+    def work(self):
+        print("Working...")
+
+class Developer(Employee):
+    def work_part(self):
+        super().work()
+
+    def work(self):
+        print("ok...")
+
+class Tester(Employee):
+    def work_test(self):
+        super().work()
+    def work(self):
+        print("test,test....")
+
+
+e = Employee()
+
+e.work()
+
+d = Developer()
+
+d.work_part()
+d.work()
+
+t = Tester()
+t.work_test()
+t.work()
