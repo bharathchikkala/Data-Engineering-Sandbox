aa2e1b0f3ef25be7f2b62cc1965cddc00168d7b4
Bharath chikkala <bharatchikkala@gmail.com>
2026-05-26 15:13:04 +0530

    Add SQL commands for managing pet shop database

--- MySQL/basics.sql
+++ MySQL/basics.sql
@@ -29,3 +29,50 @@ CREATE TABLE pastries(
 DESC pastries;
 
 DROP TABLE pastries;
+
+
+SELECT database();
+
+use pet_shop;
+
+CREATE TABLE cats3(
+	name VARCHAR(20) DEFAULT "unnamed",
+    age INT DEFAULT 99
+    );
+    
+INSERT INTO cats3 (name) VALUES ("lilly");
+
+SELECT * FROM cats3;
+
+SHOW TABLES;
+
+CREATE TABLE unique_cats(
+	cat_id INT NOT NULL PRIMARY KEY,
+	name VARCHAR(20),
+    age int
+);
+
+INSERT INTO unique_cats(cat_id,name,age) VALUES (1,"abc",7);
+
+SELECT * FROM unique_cats;
+
+INSERT INTO unique_cats(cat_id,name,age) VALUES (2,"bcd",3),(3,"hgh",3);
+
+DESC unique_cats;
+
+select database();
+
+-- learning auto_increment with PRIMARY KEY
+
+CREATE TABLE unique_cats2(
+	cat_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20),
+    age int
+);
+
+INSERT INTO unique_cats2 (name,age) VALUES ("lilly",2),("bob",3),("tiger",7);
+
+SELECT * FROM unique_cats2;
+
+DESC unique_cats2;
+
