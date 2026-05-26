8d12f3adeb41890604eecb738714997547d90cce
Bharath chikkala <bharatchikkala@gmail.com>
2026-05-26 15:14:01 +0530

    Create office database and Employees table with data
    Created a database and table for employee records, and inserted initial data.

--- MySQL/Create_Insert_Data.sql
+++ MySQL/Create_Insert_Data.sql
@@ -1,1 +1,23 @@
+CREATE DATABASE office;
 
+select database();
+
+use office;
+
+CREATE TABLE Employees(
+	ID INT PRIMARY KEY AUTO_INCREMENT,
+    Last_name VARCHAR(20) NOT NULL,
+    First_name VARCHAR(20) NOT NULL,
+    Middle_name VARCHAR(20),
+    Age INT NOT NULL,
+    Current_status VARCHAR(10) DEFAULT 'Employeed'
+);
+
+INSERT INTO Employees(Last_name,First_name,Middle_name,Age) VALUES ("Chikkala","Bharath",NULL,21),("Chikkala","Sravani","Durga",26),("Tadala",
+"Vijaya","Raju",28);
+
+DESC Employees;
+
+SELECT * FROM Employees;
+
+INSERT INTO Employees(Last_name,First_name,Middle_name,Age) VALUES ("Valavala","Satya",NULL,21);
