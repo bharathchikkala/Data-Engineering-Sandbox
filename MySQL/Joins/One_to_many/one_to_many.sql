a150f208fdf50f5ef99fb86c3f32f9c5b6911855
Bharath chikkala <bharatchikkala@gmail.com>
2026-06-09 19:39:44 +0530

    Add various SQL join examples for one-to-many

--- MySQL/Joins/One_to_many/one_to_many.sql
+++ MySQL/Joins/One_to_many/one_to_many.sql
@@ -42,3 +42,24 @@ INSERT INTO orders (order_date,price,customer_id) VALUES ('2026-06-07',1500,3);
 SELECT id FROM customers WHERE first_name = 'boy';
 
 SELECT * FROM orders WHERE customer_id = 1;
+
+-- INNER JOIN
+SELECT * FROM customers
+JOIN orders ON orders.customer_id = customers.id;
+
+-- GROUP BY
+SELECT first_name,last_name,SUM(price) FROM customers
+JOIN orders on customers.id = orders.customer_id
+GROUP BY first_name,last_name;
+
+-- LEFT JOIN
+SELECT first_name,last_name,order_date,price FROM customers
+LEFT JOIN orders ON customers.id = orders.customer_id;
+
+-- LEFT JOIN WITH GROUP BY
+SELECT first_name,last_name,IFNULL(SUM(price),0) FROM customers LEFT JOIN orders ON
+customers.id = orders.customer_id GROUP BY first_name,last_name;
+
+-- RIGHT JOIN
+SELECT * FROM customers RIGHT JOIN orders ON 
+customers.id = orders.customer_id; 
