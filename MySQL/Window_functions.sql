b5215d55c4753eed35d82a3dd14ee915b4da616c
Bharath chikkala <bharatchikkala@gmail.com>
2026-06-14 18:16:22 +0530

    Create database and employees table for window functions

--- MySQL/Window_functions.sql
+++ MySQL/Window_functions.sql
@@ -1,1 +1,11 @@
+CREATE DATABASE window_functions;
 
+USE window_functions;
+
+SELECT database();
+
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY AUTO_INCREMENT,
+    department VARCHAR(20),
+    salary INT
+);
