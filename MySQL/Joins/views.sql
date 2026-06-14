bc8c8a7d04472b95a603e1b8bad7d278db23088c
Bharath chikkala <bharatchikkala@gmail.com>
2026-06-14 18:14:33 +0530

    Add full_reviews view and rating analysis queries
    Created a view for full reviews and added various queries to analyze ratings.

--- MySQL/Joins/views.sql
+++ MySQL/Joins/views.sql
@@ -1,1 +1,18 @@
+-- VIEWS
 
+CREATE VIEW full_reviews AS
+SELECT title,rating,CONCAT(first_name,' ',last_name) AS reviewer FROM series JOIN reviews ON series.id = reviews.series_id
+JOIN reviewers ON reviewers.id = reviews.reviewer_id ORDER BY title;
+
+SELECT * FROM full_reviews;
+SELECT reviewer,MAX(rating),MIN(rating),AVG(rating) FROM full_reviews GROUP BY reviewer;
+
+SELECT title,AVG(rating) FROM full_reviews GROUP BY title HAVING COUNT(rating) > 2;
+
+SELECT * FROM full_reviews;
+
+SELECT title,AVG(rating) FROM full_reviews GROUP BY title HAVING AVG(rating) > 8;
+
+-- ROLLUP
+SELECT AVG(rating) FROM full_reviews;
+SELECT title,AVG(rating) FROM full_reviews GROUP BY title WITH ROLLUP;
