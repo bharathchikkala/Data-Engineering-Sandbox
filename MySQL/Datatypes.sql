e9d8bd250cca1c78f7292b6c50a52c3bd0e0ef59
Bharath chikkala <bharatchikkala@gmail.com>
2026-06-17 01:11:20 +0530

    Add SQL queries and insert statements for people

--- MySQL/Datatypes.sql
+++ MySQL/Datatypes.sql
@@ -20,3 +20,23 @@ INSERT INTO people (name,birthdate,birthtime,birthdt) VALUES
     ('raja','1996-11-06','08:45:20','1996-11-06 08:45:20'),
     ('ganga','2004-10-23','12:30:20','2004-10-23 12:30:20')
     ;
+
+
+TRUNCATE people;
+
+SELECT * FROM people;
+SELECT CURTIME();
+SELECT CURDATE();
+SELECT NOW();
+
+-- CREATE TABLE right_now(
+-- 	now_time TIME CURTIME(),
+--     now_date DATE CURDATE()
+-- );
+
+INSERT INTO people (name,birthdate,birthtime,birthdt) VALUES ('vyshu',CURDATE(),CURTIME(),NOW());
+
+SELECT DAYNAME('2004-12-02');
+SELECT DAYNAME('2004-10-23');
+
+SELECT name,birthdate,DAYNAME(birthdate),DAYOFYEAR(birthdate),WEEK(birthdate) AS day FROM people;
