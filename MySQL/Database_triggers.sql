bf08af07396f9c0ef8dbae8c8f8a57ff7c926784
Bharath chikkala <bharatchikkala@gmail.com>
2026-06-20 05:23:03 +0530

    Create database and trigger for user age validation

--- MySQL/Database_triggers.sql
+++ MySQL/Database_triggers.sql
@@ -1,1 +1,30 @@
+CREATE DATABASE trigger_s;
 
+USE trigger_s;
+
+CREATE TABLE users(
+username VARCHAR(100),
+age INT
+);
+
+INSERT INTO users(username,age) VALUES ('bharath',21);
+
+SELECT * FROM users;
+
+DELIMITER $$
+
+CREATE TRIGGER must_be_adult
+     BEFORE INSERT ON users FOR EACH ROW
+     BEGIN
+          IF NEW.age < 18
+          THEN
+              SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'Must be an adult!';
+          END IF;
+     END;
+$$
+DELIMITER ;
+
+INSERT INTO users(username,age) VALUES ('raja',25),('sravani',25);
+
+SELECT * FROM users;
