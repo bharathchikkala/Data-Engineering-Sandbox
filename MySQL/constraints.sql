613313537bd0f4f12dbf168ca62c8704f739963c
Bharath chikkala <bharatchikkala@gmail.com>
2026-06-20 05:20:55 +0530

    Create palindromes table with CHECK constraint
    Added a table for palindromes with a CHECK constraint and performed various operations including inserts and alterations.

--- MySQL/constraints.sql
+++ MySQL/constraints.sql
@@ -1,1 +1,26 @@
+SELECT database();
 
+use knowing_things;
+
+-- UNIQUE CONSTRAINT
+-- data which is inserting into the table should be unique
+
+
+-- CHECK CONSTRAINT
+CREATE TABLE palindromes(
+	word VARCHAR(30) CHECK(REVERSE(word) = word)
+    );
+    
+SELECT * FROM palindromes;
+
+INSERT INTO palindromes(word) VALUES ('aaaa');
+    
+SELECT * FROM palindromes;
+
+DESC palindromes;
+
+ALTER TABLE palindromes
+MODIFY word VARCHAR(20);
+
+ALTER TABLE palindromes
+CHANGE word word_in VARCHAR(70);
