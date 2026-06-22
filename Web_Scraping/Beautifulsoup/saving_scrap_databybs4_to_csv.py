3b6e10e145bf57b8f14c12f398da554e68a6005c
Bharath chikkala <bharatchikkala@gmail.com>
2026-06-22 23:46:04 +0530

    Add web scraping script to save quotes to CSV
    This script scrapes quotes from a website and saves them to a CSV file, including the quote text, author, and tags.

--- Web_Scraping/Beautifulsoup/saving_scrap_databybs4_to_csv.py
+++ Web_Scraping/Beautifulsoup/saving_scrap_databybs4_to_csv.py
@@ -1,1 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
 
+website = 'http://quotes.toscrape.com'
+result = requests.get(website)
+content = result.text
+
+soup = BeautifulSoup(content, 'html.parser')
+
+# Open new CSV file to write our data
+# encoding='utf-8' ensures special characters (like quotes/emojis) save correctly.
+with open('my_first_dataset.csv', 'w', newline='', encoding='utf-8') as file:
+    
+    writer = csv.writer(file)
+
+
+    
+    writer.writerow(['Quote', 'Author','Tags'])
+
+    
+    all_quote_boxes = soup.find_all('div', class_='quote')
+
+    
+    for box in all_quote_boxes:
+        
+        quote_text = box.find('span', class_='text').text
+        author_name = box.find('small', class_='author').text
+        quote_tags = box.find('meta', class_='keywords')['content']
+
+        writer.writerow([quote_text, author_name,quote_tags])
+
+print("Data successfully saved to dataset.csv")
