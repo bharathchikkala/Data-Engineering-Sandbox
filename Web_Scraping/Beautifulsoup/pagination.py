136f7ea70f817fb2b65d781e9499c313d99d50f6
Bharath chikkala <bharatchikkala@gmail.com>
2026-06-23 00:08:02 +0530

    Implement pagination for scraping quotes
    The script scrapes quotes from the inspirational tag on the specified website, handling pagination to load all available pages.
    pagination is simply going to next page for getting the data

--- Web_Scraping/Beautifulsoup/pagination.py
+++ Web_Scraping/Beautifulsoup/pagination.py
@@ -1,1 +1,44 @@
+# pagination is simply going to next page for data
 
+import requests
+from bs4 import BeautifulSoup
+import time
+
+base_url = 'http://quotes.toscrape.com'
+
+current_relative_url = '/tag/inspirational/'
+
+next_page_exists = True
+
+while next_page_exists:
+
+    #  base URL and the current page together
+    full_url = base_url + current_relative_url
+    print(f"\n--- Loading Page: {full_url} ---")
+
+    response = requests.get(full_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # getting data
+    all_quotes = soup.find_all('span', class_='text')
+    for quote in all_quotes:
+        print(quote.text)
+
+    # On website, the next button is inside an <li> tag with class="next"
+    next_button_box = soup.find('li', class_='next')
+
+    if next_button_box:
+        # If the box exists, find the <a> tag inside it and get href!
+        next_link = next_button_box.find('a')['href']
+
+        # Update the url so the NEXT loop goes to the new page
+        current_relative_url = next_link
+
+        print("-> Next button found! going to page...")
+        time.sleep(1) 
+      
+    else:
+        print("-> No 'Next' button found. We are in final page!")
+        next_page_exists = False
+
+print("\nAll pages scraped successfully.")
