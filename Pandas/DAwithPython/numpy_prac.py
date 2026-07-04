9a160c42cc72ab01c61a76945ed0c74c1c5e07ee
Bharath chikkala <bharatchikkala@gmail.com>
2026-07-04 23:35:47 +0530

    Add examples for 2D array, arange, and ones
    Added new NumPy array examples for 2D array, arange, and ones.

--- Pandas/DAwithPython/numpy_prac.py
+++ Pandas/DAwithPython/numpy_prac.py
@@ -6,3 +6,13 @@ arr1 = np.array([1,2,3])
 print(arr1)
 print(arr1.shape)
 print(type(arr1))
+
+arr2 = np.array([[1,2,3,20] ,[4,5,6,20],[7,8,9,20]])
+print(arr2)
+print(arr2.shape)
+
+arran = np.arange(1,10,2)
+print(arran)
+
+one = np.ones([4,5])
+print(one)
