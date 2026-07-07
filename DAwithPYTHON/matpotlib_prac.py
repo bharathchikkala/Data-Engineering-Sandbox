5c7735cd5f941a10c797102283574437f0cd2739
Bharath chikkala <bharatchikkala@gmail.com>
2026-07-07 13:35:07 +0530

    Implement subplots and bar plot in matplotlib example
    Added multiple subplots and a bar plot using matplotlib.

--- DAwithPYTHON/matpotlib_prac.py
+++ DAwithPYTHON/matpotlib_prac.py
@@ -1,1 +1,36 @@
+import matplotlib.pyplot as plt
 
+
+x = [1,2,3,4,5,10,20]
+y = [0,5,9,13,15,20,100]
+plt.subplot(2,2,1)
+plt.plot(x,y,color='green',linestyle='-',marker='*',markersize=7)
+plt.xlabel('Days')
+plt.ylabel('Result')
+plt.title('How it works')
+# plt.show()
+
+plt.subplot(2,2,2)
+z = [12,23,34,23,19,33,120]
+plt.xlabel('the day')
+plt.ylabel('res')
+plt.title('plot2')
+plt.plot(x,z,color='green',linestyle='-',marker='*',markersize=7)
+# plt.show()
+
+plt.subplot(2,2,3)
+m = [22,45,32,65,23,56,520]
+plt.title('plot3')
+plt.plot(x,m,color='green',linestyle='-',marker='*',markersize=7)
+plt.tight_layout()
+plt.show()
+
+# # bar plot
+items = ['A','B','C','D','E']
+values = [5,10,13,15,20]
+plt.title('Plot 1')
+plt.xlabel('Items')
+plt.ylabel('Values')
+# plt.subplots_adjust(top=0.85)
+plt.bar(items,values,color='green')
+plt.show()
